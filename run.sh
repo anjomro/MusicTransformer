2c81156863cab892050dbe7f972d26a558fef8c4
Pau Lozano García <paulozano.gar@gmail.com>
2022-03-27 20:20:10 +0200

    pre-trained models

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 #SBATCH -c1
 #SBATCH --gres=gpu:1,gpumem:16G
 #SBATCH -t 1-00:00
-python3 train.py -c config/train.yml config/base.yml -m dataset/
+python3 -u train.py -c config/train.yml config/base.yml -m dataset/
